8282ad14e211e6babf213f009aa56021e1624841
Oleh <158223929+olehsekirkin@users.noreply.github.com>
2024-02-13 08:14:21 -0500

    noemptycells.py

--- noemptycells.py
+++ noemptycells.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 from pathlib import Path
 
-# Load your CSV file into a DataFrame
-file_path = 'C:\\Users\\olehs\\Desktop\\output_with_technical_indicators.csv'
+# Load your .csv file into a DataFrame
+file_path = "C:\\Users\\YOURUSERNAME\\Desktop\\output_with_technical_indicators.csv"
 df = pd.read_csv(file_path)
 
 # Drop rows with missing data
@@ -13,7 +13,7 @@ df = df.dropna()
 desktop_path = str(Path.home() / "Desktop")
 
 # Save the cleaned DataFrame to the desktop
-cleaned_file_path = os.path.join(desktop_path, 'cleaned_file.csv')
+cleaned_file_path = os.path.join(desktop_path, "cleaned_file.csv")
 df.to_csv(cleaned_file_path, index=False)
 
 print(f"Rows with missing data removed. Cleaned file saved to {cleaned_file_path}")
