5744e8c01f0a15fc84843811c79c6cb286048ae1
Oleh <158223929+olehsekirkin@users.noreply.github.com>
2024-02-13 08:15:01 -0500

    ML2.py

--- ML2.py
+++ ML2.py
@@ -5,16 +5,16 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-# Load your CSV data
-file_path = 'C:\\Users\\olehs\\Desktop\\cleaned_file.csv'
+# Load your .csv data
+file_path = "C:\\Users\\YOURUSERNAME\\Desktop\\cleaned_file.csv"
 data = pd.read_csv(file_path)
 
 # Remove leading and trailing spaces from column names
 data.columns = data.columns.str.strip()
 
 # Define features (X) and target variable (y)
-features = data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'SMA', 'EMA', 'RSI', 'MACD', 'upper_band', 'lower_band', '%K', '%D', 'ATR', 'Momentum']]
-target = data['Close']
+features = data[["Open", "High", "Low", "Close", "Adj Close", "Volume", "SMA", "EMA", "RSI", "MACD", "upper_band", "lower_band", "%K", "%D", "ATR", "Momentum"]]
+target = data["Close"]
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
@@ -35,9 +35,6 @@ y_pred = rf_model.predict(X_test_scaled)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
-print(f'Mean Squared Error: {mse}')
+print(f"Mean Squared Error: {mse}")
 
 # Now you can use the trained model to make predictions on new data
-# new_data = ...  # Provide new data in the same format as your training data
-# new_data_scaled = scaler.transform(new_data)
-# predictions = rf_model.predict(new_data_scaled)
