17b3e7d23ca9ed083f527117c18f42f70ed2e584
Oleh <158223929+olehsekirkin@users.noreply.github.com>
2024-02-13 08:13:49 -0500

    feature2.py

--- feature2.py
+++ feature2.py
@@ -1,32 +1,32 @@
 import pandas as pd
 from ta.trend import ADXIndicator, CCIIndicator, EMAIndicator, MACD, PSARIndicator, STCIndicator
 
-# Load your CSV file
-input_csv_path = 'C:\\Users\\olehs\\Desktop\\preprocessed_stock_data.csv'
-output_csv_path = 'C:\\Users\\olehs\\Desktop\\feature2.csv'
+# Load your .csv file
+input_csv_path = "C:\\Users\\YOURUSERNAME\\Desktop\\preprocessed_stock_data.csv"
+output_csv_path = "C:\\Users\\YOURUSERNAME\\Desktop\\feature2.csv"
 
 df = pd.read_csv(input_csv_path)
 
 # Calculate trend indicators
-df['adx'] = ADXIndicator(df['High'], df['Low'], df['Close']).adx()
+df["adx"] = ADXIndicator(df["High"], df["Low"], df["Close"]).adx()
 
 # Manually calculate Aroon indicator
 window = 14  # You can adjust the window as needed
-high_window = df['High'].rolling(window=window, min_periods=1).apply(lambda x: x.argmax(), raw=True)
-low_window = df['Low'].rolling(window=window, min_periods=1).apply(lambda x: x.argmin(), raw=True)
+high_window = df["High"].rolling(window=window, min_periods=1).apply(lambda x: x.argmax(), raw=True)
+low_window = df["Low"].rolling(window=window, min_periods=1).apply(lambda x: x.argmin(), raw=True)
 
-df['aroon_up'] = (window - high_window) / window * 100
-df['aroon_down'] = (window - low_window) / window * 100
+df["aroon_up"] = (window - high_window) / window * 100
+df["aroon_down"] = (window - low_window) / window * 100
 
-df['cci'] = CCIIndicator(df['High'], df['Low'], df['Close']).cci()
-df['ema'] = EMAIndicator(df['Close']).ema_indicator()
-macd = MACD(df['Close'])
-df['macd'] = macd.macd()
-df['macd_signal'] = macd.macd_signal()
-df['psar'] = PSARIndicator(df['High'], df['Low'], df['Close']).psar()
-df['stc'] = STCIndicator(df['Close']).stc()
+df["cci"] = CCIIndicator(df["High"], df["Low"], df["Close"]).cci()
+df["ema"] = EMAIndicator(df["Close"]).ema_indicator()
+macd = MACD(df["Close"])
+df["macd"] = macd.macd()
+df["macd_signal"] = macd.macd_signal()
+df["psar"] = PSARIndicator(df["High"], df["Low"], df["Close"]).psar()
+df["stc"] = STCIndicator(df["Close"]).stc()
 
-# Save the result to a new CSV file on the desktop
+# Save the result to a new .csv file on the desktop
 df.to_csv(output_csv_path, index=False)
 
-print(f'Technical indicators have been calculated and saved to {output_csv_path}')
+print(f"Technical indicators have been calculated and saved to {output_csv_path}")
